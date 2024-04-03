cde4dd4dc17e36de0510c530ac33c66c6731fdbd
Greice Santana <greicesantanags528@gmail.com>
2024-04-02 22:04:20 -0300

    Feat: learning about objects

--- FundamentosJs/script.js
+++ FundamentosJs/script.js
@@ -54,13 +54,86 @@ console.log(list[4])// para retornar um item de um array é só abrir [] conchet
 list[5] = 'f'
 console.log(list)
 
-list.push('g')
+list.push('g')// adiciona um novo elemento no final do array
 console.log(list, 'push')
 
 console.log(list[list,length -1],'aqui')
 
-list.pop()
-console.log(list)
+list.pop()// remove o ultimo elemento do array
+console.log(list, 'pop')
+
+list.shift() // remove o primeiro elemento 
+console.log(list, 'shift')
+
+list.unshift() // adiciona os elementos especificados ao início de um array e retorna o novo comprimento do array. ex: 1,2,3- retorna: 4 ,1,2,3.
+console.log(list, 'unshift')
+
+//start do slice = valor do lenght anterior
+//end = valor real
+console.log(list.slice(1,4))
+
+//Objetos
+
+const product = {
+
+name: 'Regata',
+price: 49.99,
+inStock: true,
+sizes: ['P','M','G']
+
+}
+
+console.log(product.name)
+console.log(product.price)
+console.log(product.inStock)
+console.log(product.sizes)
+
+product.color = 'blue' // chama o objeto e defini o que você quer
+console.log(product)
+
+//destruturing
+const { price, inStock} = product
+
+
+console.log(price)
+console.log(inStock)
+
+//JSON
+const dog = {
+    name: 'Shark',
+    age: 5,
+    breed: 'Caramelo'
+}
+
+const jsonSt = JSON.stringify (dog)
+const jsonReset = JSON.parse(jsonSt)
+
+console.log(jsonSt)
+console.log(jsonReset)
+
+//DATA
+const data = new Date ()
+console.log(data)
+
+//Operadores 
+const a = 10
+const b = 15
+
+//aritméticos
+const soma = a + b 
+const subt = a - b
+const div = a / b 
+const mult = a * b
+const por = a % b
+
+
+console.log(soma, subt, div, mult, por, 'ari')
+
+
+
+
+
+
 
 
 
